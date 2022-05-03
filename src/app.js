e0d9ea5f7bc838d38d47ce63ca091340d9df8f42
I-r-e-n-a <101994780+I-r-e-n-a@users.noreply.github.com>
2022-05-03 21:46:37 +0300

    Added current weather functionality
    Included API
    Created function to update current weather conditions

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
+function displayTemp(response) {
+  console.log(response.data);
+  document.querySelector("#current-city").innerHTML = city;
+  document.querySelector("#current-temp").innerHTML = `${Math.round(
+    response.data.main.temp
+  )}°`;
+  document.querySelector("#current-conditions").innerHTML =
+    response.data.weather[0].description;
+  document.querySelector(
+    "#current-humidity"
+  ).innerHTML = `${response.data.main.humidity}%`;
 
+  document.querySelector("#current-wind").innerHTML = `${Math.round(
+    response.data.wind.speed
+  )} km/h`;
+}
+
+let apiKey = "1226c04775770540034fbff39a889d84";
+let city = "Madrid";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemp);
